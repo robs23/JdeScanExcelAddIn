7ebf75c52e1f84b5e921f8e41e6a967a54fdac24
robert roszak <robert.roszak@outlook.com>
2019-10-09 11:50:50 +0200

    Add project files.

---
+++ JdeScanExcelAddIn/JdeScanRibbon.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace JdeScanExcelAddIn
+{
+    partial class JdeScanRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public JdeScanRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JdeScanRibbon));
+            this.tabJdeScan = this.Factory.CreateRibbonTab();
+            this.grpJdeScan = this.Factory.CreateRibbonGroup();
+            this.btnJdeScanExport = this.Factory.CreateRibbonButton();
+            this.tabJdeScan.SuspendLayout();
+            this.grpJdeScan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabJdeScan
+            // 
+            this.tabJdeScan.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabJdeScan.Groups.Add(this.grpJdeScan);
+            this.tabJdeScan.Label = "JDE Scan";
+            this.tabJdeScan.Name = "tabJdeScan";
+            // 
+            // grpJdeScan
+            // 
+            this.grpJdeScan.Items.Add(this.btnJdeScanExport);
+            this.grpJdeScan.Label = "JDE Scan";
+            this.grpJdeScan.Name = "grpJdeScan";
+            // 
+            // btnJdeScanExport
+            // 
+            this.btnJdeScanExport.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnJdeScanExport.Image = ((System.Drawing.Image)(resources.GetObject("btnJdeScanExport.Image")));
+            this.btnJdeScanExport.Label = "Export";
+            this.btnJdeScanExport.Name = "btnJdeScanExport";
+            this.btnJdeScanExport.ShowImage = true;
+            this.btnJdeScanExport.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnJdeScanExport_Click);
+            // 
+            // JdeScanRibbon
+            // 
+            this.Name = "JdeScanRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tabJdeScan);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.JdeScanRibbon_Load);
+            this.tabJdeScan.ResumeLayout(false);
+            this.tabJdeScan.PerformLayout();
+            this.grpJdeScan.ResumeLayout(false);
+            this.grpJdeScan.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabJdeScan;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpJdeScan;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnJdeScanExport;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal JdeScanRibbon JdeScanRibbon
+        {
+            get { return this.GetRibbon<JdeScanRibbon>(); }
+        }
+    }
+}
---
+++ JdeScanExcelAddIn/JdeScanRibbon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+
+namespace JdeScanExcelAddIn
+{
+    public partial class JdeScanRibbon
+    {
+        private void JdeScanRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnJdeScanExport_Click(object sender, RibbonControlEventArgs e)
+        {
+            Workbook wb = Globals.ThisAddIn.Application.ActiveWorkbook;
+            Worksheet sht = wb.ActiveSheet;
+            MessageBox.Show(sht.Name);
+        }
+    }
+}
